f77b1947122920b9d92bdfaeca1b7b2e5f8e5c10
Victor Ukok <ukokjnr@gmail.com>
2024-05-23 21:54:38 +0100

    Fix: Path problem.

--- app/components/admindashboard/AddAreaHeader.tsx
+++ app/components/admindashboard/AddAreaHeader.tsx
@@ -8,7 +8,9 @@ interface PropsInterface {
 }
 
 function AddAreaHeader(props: PropsInterface) {
+    
     const router = useRouter();
+
     return (
         <button onClick={() => {router.back()}} className='font-bold flex justify-center items-center gap-4'><IoArrowBack /> <div>{props.title}</div></button>
     );
