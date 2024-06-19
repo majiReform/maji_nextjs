2bb72d8f540b83df738cdcd0bc692584177a216e
Victor Ukok <ukokjnr@gmail.com>
2024-06-19 15:22:46 +0100

    Fix: About us image

--- components/guests/AboutUs/Header.tsx
+++ components/guests/AboutUs/Header.tsx
@@ -4,7 +4,7 @@ function GuestAboutHeader() {
     return (
         <div className="relative flex justify-center items-center bg-black" style={{zIndex: "10"}}>
             {/* <Image src="/image 14.png" fill={true} alt="Im" className="absolute left-0 top-0" style={{zIndex: "10", position: "absolute"}} /> */}
-            <div className="h-[200px] sm:h-[320px] lg:h-[400px] lx:h-[600px] overflow-hidden">
+            <div className="h-[200px] sm:h-[320px] lg:h-[500px] lx:h-[600px] w-full overflow-hidden">
             <img src="/About US.jpg" style={{ zIndex: "20", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", width: "100%", height: "auto" }} alt="Image 17" />
             </div>
             <div style={{ zIndex: "30", backgroundColor: "rgba(0,0,0,0.5)", width: "100%", height: "100%", position: "absolute"}}></div>
