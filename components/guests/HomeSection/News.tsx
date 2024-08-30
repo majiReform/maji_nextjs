b47d0c1c67d926daaa83ce57a2148b9765435b1f
Victor Ukok <ukokjnr@gmail.com>
2024-08-30 06:20:18 +0100

    Fix: Payment

--- components/guests/HomeSection/News.tsx
+++ components/guests/HomeSection/News.tsx
@@ -47,7 +47,7 @@ function GuestResearchAndReport() {
                         <div className="w-[80%] xl:w-full shrink-0 xl:shrink bg-white relative border border-[#E6E6E6] rounded-[10px] overflow-hidden">
                             <div className="w-full h-[300px] relative z-10" style={{ backgroundImage: `url("${record.picture}")`, backgroundSize: "cover", backgroundRepeat: "no-repeat", borderRadius: "5px", backgroundPosition: "center" }}>
                                 {/* <Image src={record.picture} fill={true} alt="Thematic picture list" className="z-10" /> */}
-                                <div className="top-[20px] left-[20px] absolute z-40 bg-black text-white rounded-[30px] font-bold w-fit px-4 py-[2px]">{record.category?.split("-").map(v => v[0].toLocaleUpperCase() + v.slice(1)).join(" ")}</div>
+                                {/* <div className="top-[20px] left-[20px] absolute z-40 bg-black text-white rounded-[30px] font-bold w-fit px-4 py-[2px]">{record.category?.split("-").map(v => v[0].toLocaleUpperCase() + v.slice(1)).join(" ")}</div> */}
                             </div>
                             <div className="pt-4 flex flex-col justify-between w-full p-4 gap-4">
                                 <div className="font-bold" style={{ textWrap: "wrap" }}>{record.title!!.length > 100 ? record.title?.slice(0, 100) + "..." : record.title}</div>
